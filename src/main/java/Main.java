f6c6daf6fa6e47919b39b773259dc870e29f1744
MNOWAK1234 <tarask2002@interia.pl>
2023-12-10 15:06:20 +0100

    Implemented SL4J Logger

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,26 @@
-public class Main {
-    public static void main(String[] args) {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-        System.out.println("Hello and welcome!");
+public class Main {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        logger.info("Hello and welcome!"); // Logs at the INFO level
 
         BuildingInfo.Runner.main();
+
+        // Example debug.log
+        if (logger.isDebugEnabled()) {
+            logger.debug("Place this when debugging"); // Logs at the DEBUG level
+        }
+        //Example of logger.warn
+        //logger.warn("Possible memory leak detected");
+        //Example of logger.error
+        /*try {
+            int result = 1/0;
+            logger.info("Division result: {}", result);
+        } catch (ArithmeticException ex) {
+            logger.error("Error occurred during division: {}", ex.getMessage());
+        }*/
     }
 }
