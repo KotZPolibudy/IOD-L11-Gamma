51c413807220ee3338983d79159ec6de2a07ba3d
MNOWAK1234 <tarask2002@interia.pl>
2023-12-10 21:28:12 +0100

    Added reading json

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,27 @@
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Main {
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
+
         logger.info("Hello and welcome!"); // Logs at the INFO level
+        // Load the JSON file for testing
+        String jsonFilePath = "src/main/resources/test_building.json"; // Update with your file path
+
+        try {
+            String buildingJson = new String(Files.readAllBytes(Paths.get(jsonFilePath)));
+
+            // Example:
+            logger.info("Loaded JSON: {}", buildingJson);
+            // Perform further operations with the 'buildingJson' string
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         BuildingInfo.Runner.main();
 
